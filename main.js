cc1baba0bd5367d447f7218fba53b5c9012ec2b7
Bradley Hagger <brad_2nd@hotmail.com>
2023-01-24 12:45:01 +0000

    added styling and add and remove functionality

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 // Event Listeners
 document.getElementById("submit").addEventListener("click", () => {
     const book = new Book(document.getElementById("title").value, document.getElementById("author").value)
-
-    book.addBook(book.title)
+    book.addBook(book.getTitle(), book.getAuthor())
 })
 
 class UI{
     constructor(){
-        this.tableTitle = document.getElementById("title__container")
+        this.form_title = document.getElementById("title")
+        this.form_author = document.getElementById("author")
+        this.book_container = document.querySelector(".book__container")
     }
 
-    addBook = (title) => {
-        let h3 = document.createElement("h3")
-        h3.innerText = title
-        this.tableTitle.appendChild(h3)
+    createBook = (title, author) => {
+        let container = document.createElement("div")
+        container.classList.add("book__flex")
+
+        container.innerHTML = 
+        `<h3>${title}</h3>
+        <h3>${author}</h3>
+        <h3 id="remove">🗑️</h3>
+        `
+
+        return container
+    }
+
+    addBook = (title, author) => {
+        if(this.form_title.value.trim() == "" || this.form_author.value.trim() == "")
+            alert("Please input a title and author")
+
+        const book = this.createBook(title, author)
+        this.book_container.appendChild(book)
+        this.removeBook(book)
+    }
+
+    removeBook = (book) => {
+        book.addEventListener("click", (e) => {
+            if(e.target.id == "remove")
+                e.target.parentElement.remove()
+        })
     }
 }
 
@@ -26,5 +50,9 @@ class Book extends UI{
 
     getTitle = () => {
         return this.title
+    }
+
+    getAuthor = () => {
+        return this.author
     }
 }
